bbc35ae8dff6309a08d4e892953860d59dce910b
Jonilson Sousa <jonilsonds9@gmail.com>
2025-09-25 19:59:09 -0300

    Flags locais vs globais

--- cmd/category.go
+++ cmd/category.go
@@ -25,6 +25,8 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(categoryCmd)
 
+	categoryCmd.PersistentFlags().String("name", "", "Name of the category to create")
+
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
